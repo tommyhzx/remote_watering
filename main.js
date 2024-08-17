5d241de950f0d8b4391386a7ffa7d12ef072a5b5
tommyhzx <tommyhzx@126.com>
2024-08-17 08:48:14 +0800

    引入vant ui组件

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ import App from './App'
 
 // #ifndef VUE3
 import Vue from 'vue'
+import Vant from 'vant';
 import './uni.promisify.adaptor'
+import 'vant/lib/index.css';
 Vue.config.productionTip = false
+Vue.use(Vant)
 App.mpType = 'app'
 const app = new Vue({
   ...App
@@ -15,6 +18,8 @@ app.$mount()
 import { createSSRApp } from 'vue'
 export function createApp() {
   const app = createSSRApp(App)
+  // 全局引入Vant
+  app.use(Vant)
   return {
     app
   }
